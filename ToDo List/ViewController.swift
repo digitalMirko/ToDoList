ed676cc93470f850dc6ed42064ab55de4372ee77
Mirko Cukich <mirkocukich@gmail.com>
2016-10-31 17:33:29 -0500

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Mirko Cukich on 10/31/16.
+//  Copyright © 2016 Digital Mirko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
